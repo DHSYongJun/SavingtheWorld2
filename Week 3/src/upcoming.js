9233ab52b6370670a8d708012667a1faf9acdee3
DHSYongJun <lim.yongjun@dhs.sg>
2018-04-08 21:45:29 +0800

    Create upcoming.js

---
+++ Week 3/src/upcoming.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui-next/styles';
+import ExpansionPanel, {
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+} from 'material-ui-next/ExpansionPanel';
+import Typography from 'material-ui-next/Typography';
+import ExpandMoreIcon from 'material-ui-icons-next/ExpandMore';
+
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.primary,
+  },
+  secondaryheading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  }
+});
+
+class ControlledExpansionPanels extends React.Component {
+  state = {
+    expanded: null,
+  };
+
+  handleChange = panel => (event, expanded) => {
+    this.setState({
+      expanded: expanded ? panel : false,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { expanded } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <ExpansionPanel expanded={expanded === 'panel1'} onChange={this.handleChange('panel1')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>Basketball C Div East Zone 2nd Round</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.secondaryheading}>
+              Date: March 31 2018<br />
+              Time: 12:30PM - 2:00PM<br />
+              Location: Hwa Chong Institution<br />
+              Competing School: Hwa Chong Institution<br />
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel expanded={expanded === 'panel2'} onChange={this.handleChange('panel2')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>Table Tennis C Div East Zone 2nd Round</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.secondaryheading}>
+              Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+              maximus est, id dignissim quam.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+        <ExpansionPanel expanded={expanded === 'panel3'} onChange={this.handleChange('panel3')}>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography className={classes.heading}>Badminton C Div East Zone 2nd Round</Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Typography className={classes.secondaryheading}>
+              Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+              maximus est, id dignissim quam.
+            </Typography>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
+      </div>
+    );
+  }
+}
+
+ControlledExpansionPanels.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ControlledExpansionPanels);
